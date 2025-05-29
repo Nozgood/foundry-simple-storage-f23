fc3524f24321af473220e2967c8012be1e25a921
nowfeel <nowfeel@mac.home>
2025-05-29 10:09:21 +0200

    feat: script to deploy smart contreact

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Idenfitier: MIT
+
+pragma solidity 0.8.19;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+import {SimpleStorage} from "src/SimpleStorage.sol";
+
+
+contract DeploySimpleStorage is Script {
+    function run() external returns(SimpleStorage){
+        // startBroadCast is used to monitor on my local node what happens
+        vm.startBroadcast();
+
+        SimpleStorage simpleStorage = new SimpleStorage();
+
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
